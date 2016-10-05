f79b605b340ee1d2ddf90fcc7db5fe0a4b5d9b05
Julian Straub <jstraub@mit.edu>
2016-10-04 21:44:11 -0400

    updated realsense video class implementation to also populate device properties and frame properties json variables; removed unnecessary output

--- src/video/drivers/realsense.cpp
+++ src/video/drivers/realsense.cpp
@@ -1,5 +1,6 @@
 #include <librealsense/rs.hpp>
 #include <pangolin/video/drivers/realsense.h>
+#include <pangolin/utils/timer.h>
 #include <string>
 #include <iostream>
 
@@ -12,19 +13,49 @@ RealSenseVideo::RealSenseVideo(ImageDim dim, int fps, bool registerRGBD)
   sizeBytes = 0;
   for (int32_t i=0; i<ctx_->get_device_count(); ++i) {
     devs_.push_back(ctx_->get_device(i));
-    devs_[i]->enable_stream(rs::stream::depth, dim_.x, dim_.y, rs::format::z16, fps_);
-    StreamInfo streamD(VideoFormatFromString("GRAY16LE"), dim_.x, dim_.y, dim_.x*2, 0);
+    devs_[i]->enable_stream(rs::stream::depth, dim_.x, dim_.y,
+        rs::format::z16, fps_);
+    StreamInfo streamD(VideoFormatFromString("GRAY16LE"), dim_.x,
+        dim_.y, dim_.x*2, 0);
     streams.push_back(streamD);
 
     sizeBytes += streamD.SizeBytes();
-    devs_[i]->enable_stream(rs::stream::color, dim_.x, dim_.y, rs::format::rgb8, fps_);
-    StreamInfo streamRGB(VideoFormatFromString("RGB24"), dim_.x, dim_.y, dim_.x*3, (uint8_t*)0+sizeBytes);
+    devs_[i]->enable_stream(rs::stream::color, dim_.x, dim_.y,
+        rs::format::rgb8, fps_);
+    StreamInfo streamRGB(VideoFormatFromString("RGB24"), dim_.x,
+        dim_.y, dim_.x*3, (uint8_t*)0+sizeBytes);
     streams.push_back(streamRGB);
     sizeBytes += streamRGB.SizeBytes();
 
     devs_[i]->start();
   }
   total_frames = std::numeric_limits<int>::max();
+  UpdateProperties();
+}
+
+void RealSenseVideo::UpdateProperties() {
+  size_t numDevices = ctx_->get_device_count();
+
+  json::value& jsrealsense = device_properties["realsense"];
+  json::value& jsdevices = jsrealsense["devices"];
+  jsdevices = json::value(json::array_type,false);
+  jsdevices.get<json::array>().resize(numDevices);
+  for (size_t i=0; i<numDevices; ++i) {
+    json::value& jsdevice = jsdevices[i];
+    jsdevice["serial_number"] = GetSerial(i);
+    jsdevice["projector_power"] = GetCurrentPower(i);
+  }
+  json::value& jsstreams = jsrealsense["streams"];
+  jsstreams = json::value(json::array_type,false);
+  jsstreams.get<json::array>().resize(streams.size());
+  for (size_t i=0; i<streams.size(); ++i) {
+    json::value& jsstream = jsstreams[i];
+    jsstream["type"] = streams[i].PixFormat().format;
+  }
+
+  streams_properties = &frame_properties["streams"];
+  *streams_properties = json::value(json::array_type,false);
+  streams_properties->get<json::array>().resize(streams.size());
 }
 
 RealSenseVideo::~RealSenseVideo() {
@@ -66,8 +97,11 @@ bool RealSenseVideo::GrabNext(unsigned char* image, bool wait) {
         memcpy(out_img, devs_[i]->get_frame_data(rs::stream::depth), streams[i*2].SizeBytes());
     }
     out_img += streams[i*2].SizeBytes();
+    (*streams_properties)[2*i]["hosttime_us"] = pangolin::Time_us(pangolin::TimeNow());
+
     memcpy(out_img, devs_[i]->get_frame_data(rs::stream::color), streams[i*2+1].SizeBytes());
     out_img += streams[i*2+1].SizeBytes();
+    (*streams_properties)[2*i+1]["hosttime_us"] = pangolin::Time_us(pangolin::TimeNow());
   }
   return true;
 }
@@ -89,34 +123,35 @@ int RealSenseVideo::Seek(int frameid) {
   return -1;
 }
 
-void RealSenseVideo::GetCurrentPower(int idx, double *power) {
+double RealSenseVideo::GetCurrentPower(int idx) {
+  double power = 0;
   rs::option opt[] = {rs::option::f200_laser_power};
-  devs_[idx]->get_options(opt,1,power);
-  // printf("current power %f\n", power[0]);
+  devs_[idx]->get_options(opt,1,&power);
+  return power;
 }
 
 void RealSenseVideo::SetPower(int idx, double power){
   double power_[] = {power};
   rs::option opt[] = {rs::option::f200_laser_power};
   devs_[idx]->set_options(opt,1,power_);
-  printf("set the power!\n");
+  if (GetCurrentPower(idx) != power) {
+    std::cerr << "Problem with setting power of realsense device to " << power 
+      << std::endl;
+  }
 }
 
 void RealSenseVideo::SetPowers(double power) {
   for (int32_t i=0; i<ctx_->get_device_count(); ++i) {
     SetPower(i,power);
   }
-  printf("set all the power!\n");
 }
 
-const char* RealSenseVideo::GetSerial(int idx){
-  if (devs_.size()>0){
+std::string RealSenseVideo::GetSerial(int idx){
+  if (idx < (int)devs_.size()){
     const char* serialNumber = devs_[idx]->get_serial();
-    std::cout << "serialnumber: " << serialNumber << std::endl;
-    device_properties["serial"] = serialNumber;
-    return serialNumber;
+    return std::string(serialNumber);
   } 
-  return 0;
+  return "";
 }
 
 }
